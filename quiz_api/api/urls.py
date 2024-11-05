49938e7b9bcb51bc6ee2fffe61769c8d1b1fd94c
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-11-05 20:11:25 +0530

    update

--- quiz_api/api/urls.py
+++ quiz_api/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-  path('<str:name>', views.getMCQ, name="mcq"),
+  path('<str:name>', views.get_mcq, name="mcq"),
+  path('api/res_list/', views.get_restaurants_list, name="get_restaurants_list"),
+  path('api/res/<int:res_id>', views.get_restaurant_data, name="get_restaurant_data"),
 ]
--- quiz_api/api/views.py
+++ quiz_api/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
 import os
 import json
+from requests
 
 # Create your views here.
 def load_json_data(file_name) :
@@ -10,6 +12,18 @@ def load_json_data(file_name) :
   with open(file_path, 'r') as json_file:
     return json.load(json_file)
 
-def getMCQ(request, name):
+def get_mcq(request, name):
   data = load_json_data(f"{name}.json")
   return JsonResponse(data, safe=False)
+
+
+def get_restaurants_list(request):
+  response = requests.get("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
+
+  return JsonResponse(response.json())
+
+
+def get_restaurant_data(request, res_id):
+  response = requests.get("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu-true&lat=12.9351929&lng=77.624480699999999&restaurantId=" + res_id)
+
+  return JsonResponse(response.json())
