3a96353be7543298f16ac13b50b87fb0f25e6a28
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-12-20 15:10:22 +0530

    updates

--- quiz_api/quiz_api/settings.py
+++ quiz_api/quiz_api/settings.py
@@ -2,23 +2,25 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.environ.get("DEBUG", "") != "False"
 
-# ALLOWED_HOSTS = [os.getenv("APP_HOST"),]
+# ALLOWED_HOSTS = [os.environ.get("APP_HOST"),]
 ALLOWED_HOSTS = ['*']
 
 
