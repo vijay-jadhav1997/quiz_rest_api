85a530e18223fe5c994538be9c0abe6d22cceeb5
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-12-20 14:03:09 +0530

    new updates

--- quiz_api/quiz_api/settings.py
+++ quiz_api/quiz_api/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [os.getenv("APP_HOST"),]
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = [os.getenv("APP_HOST"),]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
