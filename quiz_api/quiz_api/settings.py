03ba94b9c889e8f8090c50fc04aab41244b29ecd
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-12-20 14:46:02 +0530

    updates

--- quiz_api/quiz_api/settings.py
+++ quiz_api/quiz_api/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # MEDIA_URLS = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
