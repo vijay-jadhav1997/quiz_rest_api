2f3d483f9cbd96502925895460176d0da321148d
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-12-20 12:47:12 +0530

    update

--- quiz_api/res_api/views.py
+++ quiz_api/res_api/views.py
@@ -7,7 +7,7 @@ import requests
 def get_restaurants_list(request):
   response = requests.get("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
 
-  print(response)
+  # print(response)
 
   return JsonResponse(response.json())
 
